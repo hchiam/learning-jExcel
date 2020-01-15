eb95ea8429f40e7231badec706988b7e9b66cc30
hchiam <hchiam@gmail.com>
2020-01-14 23:05:02 -0500

    start using examples

--- index.js
+++ index.js
@@ -4,9 +4,33 @@ const app = express();
 // be able to use files:
 app.use('/', express.static(__dirname + ''));
 
-// serve index.html at localhost:3000/
+// serve overview-demo.html at localhost:3000/
 app.get("/", (request, response) => {
   response.sendFile(__dirname + '/overview-demo.html');
+});
+
+app.get("/comments", (request, response) => {
+  response.sendFile(__dirname + '/other-examples/comments.html');
+});
+
+app.get("/formulas", (request, response) => {
+  response.sendFile(__dirname + '/other-examples/formulas.html');
+});
+
+app.get("/import", (request, response) => {
+  response.sendFile(__dirname + '/other-examples/import.html');
+});
+
+app.get("/lazy", (request, response) => {
+  response.sendFile(__dirname + '/other-examples/lazy.html');
+});
+
+app.get("/merge", (request, response) => {
+  response.sendFile(__dirname + '/other-examples/merge.html');
+});
+
+app.get("/react", (request, response) => {
+  response.sendFile(__dirname + '/other-examples/react.html');
 });
 
 // make this app actually listen for requests:
